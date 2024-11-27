3a1e4ebef75a446bb8cdf743d732c922369ed83e
Gafar Yahya <hargarx@gmail.com>
2024-11-28 00:18:32 +0300

    Day 6 19:20 till 00:20

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1964617422"
+    serviceWorkerVersion: "2883982301"
   }
 });
